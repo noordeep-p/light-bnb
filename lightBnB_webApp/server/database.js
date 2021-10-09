8c46c6e66312e1b7c325016fea101e32eb206dad
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-09 00:42:22 -0400

    rewrite given functions to use node postgres to query lightBnB db

--- lightBnB_webApp/server/database.js
+++ lightBnB_webApp/server/database.js
@@ -1,3 +1,78 @@
+const { Pool } = require('pg');
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 'lighthouse',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+/// Users
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+
+const getUserWithEmail = function(email) {
+  let user;
+  const queryString = `SELECT * FROM users WHERE email = $1`;
+
+  return pool.query(queryString, [email])
+    .then(res => {
+      if (res.rows[0]) {
+        user = res.rows[0];
+      } else {
+        user = null;
+      }
+      return user;
+    }).catch(e => console.log(e));
+};
+exports.getUserWithEmail = getUserWithEmail;
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+
+const getUserWithId = function(id) {
+  let user;
+  const queryString = `SELECT * FROM users WHERE id = $1`;
+
+  return pool.query(queryString, [id])
+    .then(res => {
+      if (res.rows[0]) {
+        user = res.rows[0];
+      } else {
+        user = null;
+      }
+      return user;
+    }).catch(e => console.log(e));
+};
+exports.getUserWithId = getUserWithId;
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+
+const addUser =  function(user) {
+  const text = 'INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *';
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(text, values)
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(e => console.log(e));
+};
+exports.addUser = addUser;
+
 /// Reservations
 
 /**
