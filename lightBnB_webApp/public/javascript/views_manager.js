c26841361c4dd5b433063db9ba584aa9631f295c
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-12 19:28:57 -0400

    create to clear views and append new reservation form to views

--- lightBnB_webApp/public/javascript/views_manager.js
+++ lightBnB_webApp/public/javascript/views_manager.js
@@ -4,40 +4,44 @@ $(() => {
 
   window.views_manager = {};
 
-  window.views_manager.show = function(item) {
+  window.views_manager.show = function(item, data = '') {
     $newPropertyForm.detach();
     $propertyListings.detach();
     $searchPropertyForm.detach();
     $logInForm.detach();
     $signUpForm.detach();
+    $newReservationForm.detach();
 
     switch (item) {
-      case 'listings':
-        $propertyListings.appendTo($main);
-        break;
-      case 'newProperty':
-        $newPropertyForm.appendTo($main);
-        break;
-      case 'searchProperty':
-        $searchPropertyForm.appendTo($main);
-        break;
-      case 'logIn':
-        $logInForm.appendTo($main);
-        break;
-      case 'signUp':
-        $signUpForm.appendTo($main);
-        break;
-      case 'error': {
-        const $error = $(`<p>${arguments[1]}</p>`);
-        $error.appendTo('body');
-        setTimeout(() => {
-          $error.remove();
-          views_manager.show('listings');
-        }, 2000);
-        
-        break;
-      }
+    case 'listings':
+      $propertyListings.appendTo($main);
+      break;
+    case 'newProperty':
+      $newPropertyForm.appendTo($main);
+      break;
+    case 'searchProperty':
+      $searchPropertyForm.appendTo($main);
+      break;
+    case 'logIn':
+      $logInForm.appendTo($main);
+      break;
+    case 'signUp':
+      $signUpForm.appendTo($main);
+      break;
+    case 'error': {
+      const $error = $(`<p>${arguments[1]}</p>`);
+      $error.appendTo('body');
+      setTimeout(() => {
+        $error.remove();
+        views_manager.show('listings');
+      }, 2000);
+      break;
     }
-  }
-  
+    case 'newReservation':
+      const dataTag = `<h4>${data}</h4>`;
+      $newReservationForm.appendTo($main);
+      $(dataTag).appendTo("#datatag");
+      break;
+    }
+  };
 });
