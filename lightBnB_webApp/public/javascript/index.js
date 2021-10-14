5325ee65f7f17949d95ecc6e25e8068779114dce
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-14 16:59:28 -0400

    update jquery to add event listener to reserve button on document instead of window load, so the page does not need to be refreshed to use the reserve button

--- lightBnB_webApp/public/javascript/index.js
+++ lightBnB_webApp/public/javascript/index.js
@@ -2,7 +2,7 @@ $(function() {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
-    $('.reserve-button').on('click', function() {
+    $(document).on('click','.reserve-button', function() {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
     });
