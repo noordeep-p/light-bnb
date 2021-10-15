b83d85b1b11c614b24bff7329824929b9f6a9add
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-15 16:30:05 -0400

    update SQL query to LEFT JOIN to allow properties will NULL review values to be displayed

--- lightBnB_webApp/server/database.js
+++ lightBnB_webApp/server/database.js
@@ -108,9 +108,9 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating, count(property_reviews.rating) as review_count
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
-
+  console.log('options:', options);
   // check what search parameter were inputted by the user and use conditionals
   // to place the WHERE/ AND statements in the query
 
@@ -120,26 +120,26 @@ const getAllProperties = function(options, limit = 10) {
   }
 
   if (options.owner_id) {
-    queryParams.push(options.owner_id);
+    queryParams.push(Number(options.owner_id));
     if (queryParams.length === 1) {
       queryString += `WHERE owner_id = $${queryParams.length} `;
+    } else {
+      queryString += `AND owner_id = $${queryParams.length} `;
     }
-    queryString += `AND owner_id = $${queryParams.length} `;
   }
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
     if (queryParams.length === 2) {
       queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
     }
-    queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
   }
 
   // once the WHERE clause is finished add all queries
 
-  queryString += `
-  GROUP BY properties.id
-  `;
+  queryString += `GROUP BY properties.id`;
 
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
@@ -151,8 +151,9 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-
+  console.log(queryString, queryParams);
   return db.query(queryString, queryParams).then(res => {
+    console.log('res.rows:', res.rows);
     return res.rows;
   }).catch(e => console.log(e));
 };
