e1102b6b25194e25f9cb80a99ba3bef0f7f73781
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-10-14 21:27:40 -0400

    add event handler to review details class

--- lightBnB_webApp/public/javascript/index.js
+++ lightBnB_webApp/public/javascript/index.js
@@ -1,10 +1,14 @@
 $(function() {
-  getAllListings().then(function( json ) {
+  getAllListings().then(function(json) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
     $(document).on('click','.reserve-button', function() {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
+    });
+    $(document).on('click', '.review_details', function() {
+      const idData = $(this).attr('id').substring(15);
+      views_manager.show('showReviews', idData);
     });
   });
 });
